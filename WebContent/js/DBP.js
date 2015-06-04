43cd2ea402d8287087199c78f799813b6d520b05
suixudongi8 <suixudongi8@qq.com>
2015-06-04 13:19:02 +0800

    BFS通信
    修改了BFS通信端口，准备与后台调试对接。

--- WebContent/js/DBP.js
+++ WebContent/js/DBP.js
@@ -4,88 +4,93 @@
 var svg = d3.select("#graph");
 
 
-function DFS(){
-	
-   d3.json("../json/dfs.json", function (error, root) {
-    if (error) {
-        return console.log(error);
-    }
-    console.log(root);
-	
-	var click_id;
-	
-	var nodes = svg.selectAll("circle")
-	               .on("dblclick", function (d, i) {
-                        click_id = d.id;
-                    });
+function DFS() {
+        $.get("../json/DFS.json", { whichDataSet: crntDataSet, filename: "DFS.json" }, function (dataDFS, status) {
+                if (status != "success") {
+                        ///////////////////////////////////////////////////////
+                        //                  提醒用户没有加载成功                      //
+                        ///////////////////////////////////////////////////////
+                        return console.log(status);
+                }
 
-    // 重绘连接线
-    var edges_line = svg.selectAll("line");
-    	
-    for(var i = 0; i < edges_line[0].length; i++){
-	    edges_line[0][i].style.stroke = "#ccc";
-		for(var j = 0; j < root.length; j++){
-			if(edges_line[0][i].__data__.source.id == root[j].source && edges_line[0][i].__data__.target.id == root[j].target){
-				//setInterval(function(){}, 10000);
-				edges_line[0][i].style.stroke = "red";
-				//edges_line[0][i].style.stroke = "red";
-				//edges_line[0][i].innerHTML = "<animate attributeName= \"x1\" attributeType=\"auto\" \
-				                     //              begin=\"0s\" dur=\"5s\" from= x1 to= x2 /> ";
-				
-				/*edges_line[0][i].enter().append("animate")
-				                .attr("begin", "3s")
-								.attr("dur", "3s");*/
-			}
-		}
-	}
-});
-	
+                var click_id;
+
+                var nodes = svg.selectAll("circle")
+                               .on("dblclick", function (d, i) {
+                                       click_id = d.id;
+                               });
+
+                // 重绘连接线
+                var edges_line = svg.selectAll("line");
+
+                for (var i = 0; i < edges_line[0].length; i++) {
+                        edges_line[0][i].style.stroke = "#ccc";
+                        for (var j = 0; j < dataDFS.length; j++) {
+                                if (edges_line[0][i].__data__.source.id == dataDFS[j].source && edges_line[0][i].__data__.target.id == dataDFS[j].target) {
+                                        //setInterval(function(){}, 10000);
+                                        edges_line[0][i].style.stroke = "red";
+                                        //edges_line[0][i].style.stroke = "red";
+                                        //edges_line[0][i].innerHTML = "<animate attributeName= \"x1\" attributeType=\"auto\" \
+                                        //              begin=\"0s\" dur=\"5s\" from= x1 to= x2 /> ";
+
+                                        /*edges_line[0][i].enter().append("animate")
+                                                        .attr("begin", "3s")
+                                                                        .attr("dur", "3s");*/
+                                }
+                        }
+                }
+        });
+
 }
 
 
 
-function BFS(){
-	
-	d3.json("../json/bfs.json", function (error, root) {
-    if (error) {
-        return console.log(error);
-    }
-    console.log(root);
+function BFS() {
 
-    // 重绘连接线
-    var edges_line = svg.selectAll("line");
-    	
-    for(var i = 0; i < edges_line[0].length; i++){
-		edges_line[0][i].style.stroke = "#ccc";
-		for(var j = 0; j < root.length; j++){
-			if(edges_line[0][i].__data__.source.id == root[j].source && edges_line[0][i].__data__.target.id == root[j].target){
-				edges_line[0][i].style.stroke = "red";
-			}
-		}
-	}
-});
-	
+        $.get("../json/BFS.json", { whichDataSet: crntDataSet, filename: "BFS.json" }, function (dataBFS, status) {
+                if (status != "success") {
+                        ///////////////////////////////////////////////////////
+                        //                  提醒用户没有加载成功                      //
+                        ///////////////////////////////////////////////////////
+                        return console.log(status);
+                }
+
+                // 重绘连接线
+                var edges_line = svg.selectAll("line");
+
+                for (var i = 0; i < edges_line[0].length; i++) {
+                        edges_line[0][i].style.stroke = "#ccc";
+                        for (var j = 0; j < dataBFS.length; j++) {
+                                if (edges_line[0][i].__data__.source.id == dataBFS[j].source && edges_line[0][i].__data__.target.id == dataBFS[j].target) {
+                                        edges_line[0][i].style.stroke = "red";
+                                }
+                        }
+                }
+        });
+
 }
 
-function Prim(){
-	
-	d3.json("../json/prim.json", function (error, root) {
-    if (error) {
-        return console.log(error);
-    }
-    console.log(root);
+function Prim() {
 
-    // 重绘连接线
-    var edges_line = svg.selectAll("line");
-    	
-    for(var i = 0; i < edges_line[0].length; i++){
-		edges_line[0][i].style.stroke = "#ccc";
-		for(var j = 0; j < root.length; j++){
-			if(edges_line[0][i].__data__.source.id == root[j].source && edges_line[0][i].__data__.target.id == root[j].target){
-				edges_line[0][i].style.stroke = "red";
-			}
-		}
-	}
-});
-	
+        $.get("../json/Prim.json", { whichDataSet: crntDataSet, filename: "Prim.json" }, function (dataPrim, status) {
+                if (status != "success") {
+                        ///////////////////////////////////////////////////////
+                        //                  提醒用户没有加载成功                      //
+                        ///////////////////////////////////////////////////////
+                        return console.log(status);
+                }
+
+                // 重绘连接线
+                var edges_line = svg.selectAll("line");
+
+                for (var i = 0; i < edges_line[0].length; i++) {
+                        edges_line[0][i].style.stroke = "#ccc";
+                        for (var j = 0; j < dataPrim.length; j++) {
+                                if (edges_line[0][i].__data__.source.id == dataPrim[j].source && edges_line[0][i].__data__.target.id == dataPrim[j].target) {
+                                        edges_line[0][i].style.stroke = "red";
+                                }
+                        }
+                }
+        });
+
 }
